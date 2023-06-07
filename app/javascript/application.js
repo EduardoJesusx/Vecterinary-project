adcfb8a3e4617fb5280c5a61ec6cafc722897f49
EduardoJesusx <srduduca@gmail.com>
2023-06-07 03:18:08 -0300

    resolving problem turbo-frame

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.querySelectorAll('form').forEach(function (el) {
+  el.dataset.turbo = false
+})
